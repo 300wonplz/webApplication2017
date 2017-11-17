210ae598539a52d94b4ec2e7fce5bf76dfb990d4
300wonplz <whgudals159@naver.com>
2017-11-18 01:22:47 +0900

    JHM

--- lab7/sql.php
+++ lab7/sql.php
@@ -5,16 +5,41 @@
     <meta charset="utf-8" />
   </head>
   <body>
+    <form action="./sql.php" method="post">
+      <div>
+        <label>dbname:</label>
+        <input type="text" id="dbname" name="dbname" />
+      </div>
+      <div>
+        <label>query:</label>
+        <input type="text" id="query" name="query" />
+      </div>
+
+      <div>
+        <button type="submit">submit</button>
+      </div>
+    </form>
+
     <?php
-      $db = new PDO("mysql:dbname=college", "root", "root");
-      $rows = $db->query("select * from student where major='컴퓨터공학'");
+      $db = new PDO("mysql:dbname=".$_POST["dbname"], "root", "root");
+      $rows = $db->query($_POST["query"]);
     ?>
     <ul>
     <?php
     foreach ($rows as $row) {
-    ?>
-        <li><?=$row["name"]?></li>
-    <?php
+      ?>
+      <li>
+      <?php
+        foreach ($row as $key => $value) {
+          if(!is_integer($key)){
+          ?>
+            <?=" ".$value?>
+          <?php
+          }
+        }
+      ?>
+    </li>
+      <?php
     }
      ?>
    </ul>
